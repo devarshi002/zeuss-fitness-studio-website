7d0f6ac62a32c1a46727a710ccbde1aaae29b2f8
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-02-21 02:29:09 +0530

    first commit

--- gym-management/server/server.js
+++ gym-management/server/server.js
@@ -23,4 +23,5 @@ app.get('/', (req, res) => {
     res.send("Gym API running 🚀");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
